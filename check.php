bd6bafeee0abb31ae5698256cf1da1da10726bf3
teamoo24 <601107w@gmail.com>
2019-12-12 13:26:49 +0900

    アンケートの基本枠完成

--- check.php
+++ check.php
@@ -9,9 +9,34 @@
 	<?php
 		# phpでは$を使って変数宣言
 		$nickname = $_POST['nickname'];
-		print 'ようこそ';
-		print $nickname;
-		print '様'
+		$email = $_POST['email'];
+		$goiken = $_POST['goiken'];
+		# if文 (普通のif文のように使えば良い)
+		# if(true or false) {	
+		# } else {
+		# }
+		if($nickname == '') {
+			print'ニックネームが入力されていません<br>';
+		} else {
+			print 'ようこそ';
+			print $nickname;
+			print '様';
+			print '<br>';
+		}
+		if($email == '') {
+			print'メールアドレスが設定されていません<br>';
+		} else {
+			print 'メールアドレス : ';
+			print $email;
+			print '<br>';
+		}
+		if($goiken == '') {
+			print'ご意見が登録されていません。';
+		} else {
+			print 'ご意見『';
+			print $goiken;
+			print '』<br>';
+		}
 	?>
 </body>
 </html>
